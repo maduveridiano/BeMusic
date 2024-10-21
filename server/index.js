c1c3dc0c13533078b036a4c28f5191c590693e1d
maduveridiano <dudqaa13@gmail.com>
2024-10-21 09:46:09 -0300

    :sparkles: Add mobile app component and update server routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 import Express from 'express'
 
 const app = Express()
+app.use(Express.json())
 
-app.get('/pegar', (req, res) => {
+
+app.get('/login', (req, res) => {
     res.send('Hello World!')
 })
 
-
+app.post('/cadastro', (req, res) => {
+    try{
+        //verificar se os campos estão preenchidos
+        const {username, email, password, birthdate} = req.body
+    if(!username || !email || !password|| !birthdate){
+        res.status(406).send('Preencha todos os campos')
+        return
+    }
+    res.status(201).send('Cadastro realizado com sucesso')
+    }catch(err){
+        console.log(err)
+    }
+})
 
 
 app.listen(3000)
